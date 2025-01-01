76e75eaaf133c8ece73ea991f664fa5cdbd70801
fajarhdev <fajarh247@gmail.com>
2025-01-01 13:39:18 +0700

    change in db connection

--- config/database.js
+++ config/database.js
@@ -12,10 +12,26 @@ require("dotenv").config();
 // 		logging: false,
 // 	}
 // );
-const sequelize = new Sequelize("surat", "postgres", "admin", {
-	host: "localhost",
-	port: 5432,
+
+const sequelize = new Sequelize({
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 	dialect: "postgres",
-	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: true, // Ensure SSL is enabled
+			rejectUnauthorized: false, // Optionally, set this to true if you have the server certificate
+		},
+	},
+	logging: false, // You can enable this for debugging
 });
+// const sequelize = new Sequelize("surat", "postgres", "admin", {
+// 	host: "localhost",
+// 	port: 5432,
+// 	dialect: "postgres",
+// 	logging: false,
+// });
 module.exports = sequelize;
