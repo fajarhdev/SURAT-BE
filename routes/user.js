d9abc3ccf5a6a0b78d2a1eb16325b4f5ec45e971
fajarhdev <fajarh247@gmail.com>
2025-01-11 23:51:19 +0700

    add get user

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,11 @@
-const { createUserController, updateUserController, deleteUserController } = require("../src/controller/userController");
+const { createUserController, updateUserController, deleteUserController, getUserController} = require("../src/controller/userController");
 const verifyToken = require("../src/middleware/verifytoken");
 const express = require("express");
-const {getUserService} = require("../src/service/user");
 const router = express.Router();
 
 router.post("/create", verifyToken, createUserController);
 router.patch("/update/:id", verifyToken, updateUserController);
 router.delete("/delete/:id", verifyToken, deleteUserController);
-router.get("/get", verifyToken, getUserC)
+router.get("/get", verifyToken, getUserController)
 
 module.exports = router;
