998c81a627f9ba40a3d93899eacf03ed4868a809
fajarhdev <fajarh247@gmail.com>
2025-03-19 21:52:42 +0700

    fix bug multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-const uploadDir = path.join(__dirname, "public/upload");
+const uploadDir = path.join(__dirname, "../..", "public/upload");
 
 if (!fs.existsSync(uploadDir)) {
 	fs.mkdirSync(uploadDir, { recursive: true });
